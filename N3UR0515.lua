1468c1bef1d1a5f6fe1ab0fb19f3723939f56d0c
anark17r-boop <anark17r@gmail.com>
2025-10-27 17:25:37 +0300

    Update N3UR0515.lua

--- N3UR0515.lua
+++ N3UR0515.lua
@@ -1,16 +1,11 @@
--- Flux UI Version
-local Flux = loadstring(game:HttpGet('https://raw.githubusercontent.com/ImagineProUser/Flux-UI-Library/main/Library.lua'))()
+local Venus = loadstring(game:HttpGet('https://raw.githubusercontent.com/hacker6060/venus-ui-library/main/source.lua'))()
 
-local Window = Flux:Window("C00lKid Exploit", "V11", "by Colin")
+local Window = Venus:New({
+    Name = "C00lKid Exploit V12",
+    Position = UDim2.new(0.5, 0, 0.5, 0)
+})
 
-Flux:Button("Тест скорости", function()
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
-end)
-
-Flux:Slider("Скорость", 16, 500, 16, function(v)
+local Main = Window:Tab("Главные")
+Main:Slider("Скорость", 16, 500, 16, function(v)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
-end)
-
-Flux:Slider("Прыжок", 50, 500, 50, function(v)
-    game.Players.LocalPlayer.Character.Humanoid.JumpPower = v
 end)
