af81d052fde687c92536b7e2cbc713f35b4e47c6
anark17r-boop <anark17r@gmail.com>
2025-10-27 20:07:31 +0300

    Create InvSi.lua

---
+++ InvSi.lua
@@ -0,0 +1,12 @@
+-- C00lKid Premium ESP V3 - Luxury Edition
+local Players = game:GetService("Players")
+local CurrentCamera = game:GetService("Workspace").CurrentCamera
+local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
+local TweenService = game:GetService("TweenService")
+local LocalPlayer = Players.LocalPlayer
+
+local Inbutton = Instance.new("InvButton")
+Inbutton.Name = "InvButton"
+InvButton.Parent = game:GetService("CoreGui")
+
